3b035963feb40a29c83cc2eb89c38d68ded89fce
tittioAngel <44965659+tittioAngel@users.noreply.github.com>
2022-05-17 21:54:36 +0200

    Implementato in parte evento e recensione, orario mi sembra non ci serva più nulla

--- Entity/ERecensione.php
+++ Entity/ERecensione.php
@@ -1,30 +1,111 @@
 <?php
 class ERecensione {
-	/**
-	* Identificativo (da capire come trattare) della singola recensione
-	* @AttributeType int
-	*/
-	private $id;
-	/**
-	* Didascalia della recensione
-	* @AttributeType string
-	*/
-	private $testo;
-	/**
-	* Compreso tra 1 e 5
-	* @AttributeType float
-	*/
-	private $voto;
-	/**Identificativo del utente che ha scritto la recensione*/
-	//private $utente;
-	//private $locale;
-	
-	public function __construct($text,$palle,/*$commentatore,$vittima*/){
-		$this->id=rand(0,1000);
-		$this->testo=$text;
-		$this->voto=$palle;
-		/*$this->utente= $commentatore;
-		$this->locale= $vittima;*/
-	}
+
+    private EUserRegistrato $utente;
+    private string $titolo;
+    private string $descrizione;
+    private int $voto;
+    private DateTime $data;
+
+    /**
+     * @param EUserRegistrato $utente
+     * @param string $titolo
+     * @param string $descrizione
+     * @param int $voto
+     * @param DateTime $data
+     */
+    public function __construct(EUserRegistrato $utente, string $titolo, string $descrizione, int $voto, DateTime $data)
+    {
+        $this->utente = $utente;
+        $this->titolo = $titolo;
+        $this->descrizione = $descrizione;
+        $this->voto = $voto;
+        $this->data = $data;
+    }
+
+    /**
+     * @return EUserRegistrato
+     */
+    public function getUtente(): EUserRegistrato
+    {
+        return $this->utente;
+    }
+
+    /**
+     * @param EUserRegistrato $utente
+     */
+    public function setUtente(EUserRegistrato $utente): void
+    {
+        $this->utente = $utente;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitolo(): string
+    {
+        return $this->titolo;
+    }
+
+    /**
+     * @param string $titolo
+     */
+    public function setTitolo(string $titolo): void
+    {
+        $this->titolo = $titolo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescrizione(): string
+    {
+        return $this->descrizione;
+    }
+
+    /**
+     * @param string $descrizione
+     */
+    public function setDescrizione(string $descrizione): void
+    {
+        $this->descrizione = $descrizione;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVoto(): int
+    {
+        return $this->voto;
+    }
+
+    /**
+     * @param int $voto
+     */
+    public function setVoto(int $voto): void
+    {
+        $this->voto = $voto;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getData(): DateTime
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param DateTime $data
+     */
+    public function setData(DateTime $data): void
+    {
+        $this->data = $data;
+    }
+
+
+    
+
+
 }
 ?>
