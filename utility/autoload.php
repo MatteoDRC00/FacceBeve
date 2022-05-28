dccd91601de30b3e2eabaa0b8f0918b76f0b31ed
RamaxSon <zanzaro197@gmail.com>
2022-05-28 18:19:58 +0200

    Da completare, ma tanto lo devo fare io ehehehehhe

---
+++ utility/autoload.php
@@ -0,0 +1,33 @@
+<?php
+
+function my_autoload($className) {
+
+    $firstLetter = $className[0];
+    switch ($firstLetter) {
+        case 'E':
+            include_once( 'Entity/'. $className . '.php' );
+            break;
+
+        case 'F':
+            include_once( 'Foundation/'. $className . '.php' );
+            break;
+
+        case 'V':
+            include_once( 'View/'. $className . '.php' );
+            break;
+
+        case 'C':
+            include_once( 'Controller/'. $className . '.php' );
+            break;
+
+        case 'I':
+            include_once( $className . '.php' );
+            break;
+
+    }
+  }
+}
+
+spl_autoload_register('my_autoload');
+
+?>
