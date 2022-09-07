f1294921bab9c2c164f0382463dc31520cb3a41d
RamaxSon <zanzaro197@gmail.com>
2022-09-07 20:34:38 +0200

    ma chi siamo

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 require_once 'utility/autoload.php';
 require_once 'StartSmarty.php';
 
-$utente = new EUtente("123","drking","ut1","USER2","utente1@mail.it");
+$utente = new EUtente("123","sfdgu","ut1","USER4","utente1@mail.it");
+$utente->Iscrizione();
 
 $id = FUtente::store($utente);
 
