c41c95e0631cce033a9cc2d58e5c05a046ecc46d
RamaxSon <zanzaro197@gmail.com>
2022-09-08 14:28:58 +0200

    Ho spostato delle cose pt.1

---
+++ Controller/CProfilo.php
@@ -0,0 +1,12 @@
+<?php
+require_once "autoloader.php";
+require_once "utility/USession.php";
+
+/**
+ * Classe per la gestione delle operazioni all'interno dell'area personale dell'utente.
+ * @package Controller
+ */
+class CProfilo
+{
+
+}
