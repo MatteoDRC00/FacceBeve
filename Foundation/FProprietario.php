bce23945efe435698f3cd394df55ca666796ae10
MatteoDRC00 <matteo.dirussociccarelli@student.univaq.it>
2022-09-21 16:05:35 +0200

    Update FProprietario.php

--- Foundation/FProprietario.php
+++ Foundation/FProprietario.php
@@ -5,34 +5,47 @@
  * @author Gruppo 8
  * @package Foundation
  */
-class FProprietario{
+class FProprietario
+{
 
-    /** classe Foundation */
-    private static $class="FProprietario";
+    /**
+     * Classe Foundation
+     * @var string
+     */
+    private static $class = "FProprietario";
 
-	/** tabella con la quale opera nel DB */
-    private static $table="Proprietario";
+    /**
+     * Tabella con la quale opera nel DB
+     * @var string
+     */
+    private static $table = "Proprietario";
 
-    /** valori della tabella nel DB */
-    private static $values="(:username,:nome,:cognome,:email,:password,:idImg)";
+    /**
+     * Valori della tabella nel DB
+     * @var string
+     */
+    private static $values = "(:username,:nome,:cognome,:email,:password,:idImg)";
 
-    /** costruttore */
-    public function __construct(){
-
+    /**
+     * Costruttore della classe
+     */
+    public function __construct()
+    {
     }
 
     /**
-    * metodo che lega gli attributi del Proprietario da inserire con i parametri della INSERT
-    * @param PDOStatement $stmt
-    * @param EProprietario $proprietario
-    */
-    public static function bind(PDOStatement $stmt, EProprietario $proprietario){
+     * Metodo che lega gli attributi del Proprietario da inserire con i parametri della INSERT
+     * @param PDOStatement $stmt
+     * @param EProprietario $proprietario
+     */
+    public static function bind(PDOStatement $stmt, EProprietario $proprietario)
+    {
         $stmt->bindValue(':username', $proprietario->getUsername(), PDO::PARAM_STR);
-        $stmt->bindValue(':nome',$proprietario->getNome(), PDO::PARAM_STR);
-        $stmt->bindValue(':cognome',$proprietario->getCognome(), PDO::PARAM_STR);
+        $stmt->bindValue(':nome', $proprietario->getNome(), PDO::PARAM_STR);
+        $stmt->bindValue(':cognome', $proprietario->getCognome(), PDO::PARAM_STR);
         $stmt->bindValue(':email', $proprietario->getEmail(), PDO::PARAM_STR);
         $stmt->bindValue(':password', md5($proprietario->getPassword()), PDO::PARAM_STR);
-        if($proprietario->getImgProfilo() != null)
+        if ($proprietario->getImgProfilo() != null)
             $stmt->bindValue(':idImg', $proprietario->getImgProfilo()->getId());
         else
             $stmt->bindValue(':idImg', null);
@@ -40,137 +53,146 @@ class FProprietario{
     }
 
     /**
-    * metodo che restituisce il nome della classe per la costruzione delle query
-    * @return string $class Nome della classe
-    */
-    public static function getClass(){
+     * Metodo che restituisce il nome della classe per la costruzione delle query
+     * @return string
+     */
+    public static function getClass()
+    {
         return self::$class;
     }
 
     /**
-    * metodo che restituisce il nome della tabella per la costruzione delle query
-    * @return string $table Nome della tabella
-    */
-    public static function getTable(){
+     * Metodo che restituisce il nome della tabella per la costruzione delle query
+     * @return string
+     */
+    public static function getTable()
+    {
         return self::$table;
     }
 
     /**
-    * metodo che restituisce l'insieme dei valori per la costruzione delle query
-    * @return string $values Nomi delle colonne della tabella
-    */
-    public static function getValues(){
+     * Metodo che restituisce l'insieme dei valori per la costruzione delle query
+     * @return string
+     */
+    public static function getValues()
+    {
         return self::$values;
     }
 
     /**
+     * Metodo che permette il salvataggio di un Proprietario nel DB
      * @param EProprietario $proprietario
      * @return false|string|null
      */
-    public static function store(EProprietario $proprietario){
+    public static function store(EProprietario $proprietario)
+    {
         $db = FDB::getInstance();
         return $db->store(static::getClass(), $proprietario);
     }
 
     /**
-     * Ritorna tutte i proprietari di locali presenti sul db
-     * @return object $rec Recensione
-     */
-    public static function loadAll() {
-        $proprietario= null;
-        $db = FDB::getInstance();
-        list($result,$num) =$db->getAll("proprietario");
-        if(($result!=null) && ($num == 1)) {
-            $proprietario = new EProprietario($result['nome'],$result['cognome'], $result['email'], $result['username'], $result['password']); //Carica un Proprietario dal database
-            $proprietario->setImgProfilo(FImmagine::loadByField('id', $result['idImg']));
-        }else{
-            if(($result!=null) && ($num > 1)){
-                $proprietario = array();
-                for($i=0; $i<count($result); $i++){
-                    $proprietario[$i] = new EProprietario($result[$i]['nome'],$result[$i]['cognome'], $result[$i]['email'], $result[$i]['username'], $result[$i]['password']); //Carica un array di oggetti Proprietario dal database
-                    $proprietario[$i]->setImgProfilo(FImmagine::loadByField('id', $result[$i]['idImg']));
-                }
-            }
-        }
-        return $proprietario;
-    }
-
-
-
-    /**
-     * @param $field
-     * @param $id
-     * @return array|EProprietario
-     */
-    public static function loadByField($field, $id){
-        $proprietario = null;
-        $db = FDB::getInstance();
-        list($result,$num) = $db->load(static::getClass(), $field, $id);
-        if(($result!=null) && ($num == 1)) {
-            $proprietario = new EProprietario($result['nome'],$result['cognome'], $result['email'], $result['username'], $result['password']); //Carica un Proprietario dal database
-            $proprietario->setImgProfilo(FImmagine::loadByField('id', $result['idImg']));
-        }else{
-            if(($result!=null) && ($num > 1)){
-                $proprietario = array();
-        	    for($i=0; $i<count($result); $i++){
-                    $proprietario[$i] = new EProprietario($result[$i]['nome'],$result[$i]['cognome'], $result[$i]['email'], $result[$i]['username'], $result[$i]['password']); //Carica un array di oggetti Proprietario dal database
-                    $proprietario[$i]->setImgProfilo(FImmagine::loadByField('id', $result[$i]['idImg']));
-                }
-            }
-        }
-        return $proprietario;
-    }
-
-    /**
-     * metodo che verifica l'esistenza di un Proprietario nel DB considerato un attributo
+     * Metodo che verifica l'esistenza di un Proprietario nel DB dato un attributo
      * @param string $attributo
      * @param string $valore
      * @return bool
      */
-    public static function exist(string $attributo,string $valore) {
+    public static function exist(string $attributo, string $valore)
+    {
         $db = FDB::getInstance();
-        $result = $db->exist(static::getClass(), $attributo, $valore);
-        if($result!=null)
-            return true;
-        else
-            return false;
+        return $db->exist(static::getClass(), $attributo, $valore);
     }
 
     /**
-     * metodo che aggiorna il valore di un attributo del Proprietario sul DB data la chiave primaria
+     * Metodo che aggiorna il valore di un attributo del Proprietario sul DB data la chiave primaria
      * @param string $attributo
      * @param string $newvalue
      * @param string $attributo_pk
      * @param string $value_pk
      * @return bool
      */
-    public static function update(string $attributo, string $newvalue, string $attributo_pk, string $value_pk){
-        $db=FDB::getInstance();
-        $result = $db->update(static::getClass(), $attributo, $newvalue, $attributo_pk, $value_pk);
-        if($result)
-            return true;
-        else
-            return false;
+    public static function update(string $attributo, string $newvalue, string $attributo_pk, string $value_pk)
+    {
+        $db = FDB::getInstance();
+        return $db->update(static::getClass(), $attributo, $newvalue, $attributo_pk, $value_pk);
     }
 
     /**
+     * Metodo che elimina un Proprietario dal DB
      * @param string $attributo
      * @param string $valore
      * @return bool
      */
-    public static function delete(string $attributo, string $valore){
-        $db=FDB::getInstance();
-        $result = $db->delete(static::getClass(), $attributo, $valore);
-        if($result)
-            return true;
-        else
-            return false;
+    public static function delete(string $attributo, string $valore)
+    {
+        $db = FDB::getInstance();
+        return $db->delete(static::getClass(), $attributo, $valore);
     }
 
-    public static function verificaLogin($user, $pass) {
+    /**
+     * Metodo che carica Proprietario dal DB dato il valore di un attributo
+     * @param string $attributo
+     * @param string $valore
+     * @return array|EProprietario
+     */
+    public static function loadByField(string $attributo, string $valore)
+    {
+        $proprietario = null;
+        $db = FDB::getInstance();
+        list($result, $num) = $db->load(static::getClass(), $attributo, $valore);
+        if (($result != null) && ($num == 1)) {
+            $proprietario = new EProprietario($result['nome'], $result['cognome'], $result['email'], $result['username'], $result['password']); //Carica un Proprietario dal database
+            $img_profilo[0] = FImmagine::loadByField('id', $result['idImg']);
+            $proprietario->setImgProfilo($img_profilo);
+        } else {
+            if (($result != null) && ($num > 1)) {
+                $proprietario = array();
+                for ($i = 0; $i < count($result); $i++) {
+                    $proprietario[$i] = new EProprietario($result[$i]['nome'], $result[$i]['cognome'], $result[$i]['email'], $result[$i]['username'], $result[$i]['password']); //Carica un array di oggetti Proprietario dal database
+                    $img_profilo[0] = FImmagine::loadByField('id', $result[$i]['idImg']);
+                    $proprietario[$i]->setImgProfilo($img_profilo);
+                }
+            }
+        }
+        return $proprietario;
+    }
+
+    /**
+     * Metodo che restituisce la lista di tutti i proprietari
+     * @return array
+     */
+    public static function loadAllProprietari()
+    {
+        $proprietario = array();
+        $db = FDB::getInstance();
+        list($result, $num) = $db->getAll("Proprietario");
+        if (($result != null) && ($num == 1)) {
+            $proprietario[0] = new EProprietario($result['nome'], $result['cognome'], $result['email'], $result['username'], $result['password']); //Carica un Proprietario dal database
+            $img_profilo[0] = FImmagine::loadByField('id', $result['idImg']);
+            $proprietario[0]->setImgProfilo($img_profilo);
+        } else {
+            if (($result != null) && ($num > 1)) {
+                for ($i = 0; $i < count($result); $i++) {
+                    $proprietario[$i] = new EProprietario($result[$i]['nome'], $result[$i]['cognome'], $result[$i]['email'], $result[$i]['username'], $result[$i]['password']); //Carica un array di oggetti Proprietario dal database
+                    $img_profilo[0] = FImmagine::loadByField('id', $result[$i]['idImg']);
+                    $proprietario[$i]->setImgProfilo($img_profilo);
+                }
+            }
+        }
+        return $proprietario;
+    }
+
+
+    /**
+     * Metodo che verifica se esiste un Proprietario date le credenziali
+     * @param $user
+     * @param $pass
+     * @return array|EProprietario|null
+     */
+    public static function verificaLogin($user, $pass)
+    {
         $db = FDB::getInstance();
         $proprietario = $db->loadVerificaAccesso($user, $pass, static::getClass());
-        if(!empty($proprietario))
+        if (!empty($proprietario))
             return self::loadByField("username", $proprietario["username"]);
         else
             return null;
