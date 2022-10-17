e4774531312db245aafc2a40cafb8e491deb1787
MatteoDRC00 <matteo.dirussociccarelli@student.univaq.it>
2022-10-17 15:34:14 +0200

    okok

--- Smarty/templates_c/30df5ca2fc2aed6ba9b9c2ca893fe37b5c7fbd9e_0.file.error.tpl.php
+++ Smarty/templates_c/30df5ca2fc2aed6ba9b9c2ca893fe37b5c7fbd9e_0.file.error.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.0, created on 2022-09-28 17:47:48
+/* Smarty version 4.2.0, created on 2022-10-17 15:08:24
   from 'C:\xampp\htdocs\FacceBeve\template\error.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.0',
-  'unifunc' => 'content_63346ca48c7cc7_11322944',
+  'unifunc' => 'content_634d53c88d96c8_87251131',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '30df5ca2fc2aed6ba9b9c2ca893fe37b5c7fbd9e' => 
     array (
       0 => 'C:\\xampp\\htdocs\\FacceBeve\\template\\error.tpl',
-      1 => 1664377115,
+      1 => 1666012070,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_63346ca48c7cc7_11322944 (Smarty_Internal_Template $_smarty_tpl) {
+function content_634d53c88d96c8_87251131 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -80,7 +80,7 @@ function content_63346ca48c7cc7_11322944 (Smarty_Internal_Template $_smarty_tpl)
 
 </main><!-- End #main -->
 
-<!-- ======= Footer ======= -->
+<!--
 <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
 
     <div class="container">
@@ -88,14 +88,11 @@ function content_63346ca48c7cc7_11322944 (Smarty_Internal_Template $_smarty_tpl)
             &copy; Copyright <strong><span>Moderna</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
-            <!-- All the links in the footer should remain intact. -->
-            <!-- You can delete the links only if you purchased the pro version. -->
-            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/ -->
+
             Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
         </div>
     </div>
-</footer><!-- End Footer -->
+</footer-->
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
